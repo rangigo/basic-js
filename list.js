c837afb4d98ee4c42cfe9ec050001a07638a8a42
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-05-14 11:21:33 +0300

    Comments/recursive function for list.js

--- list.js
+++ list.js
@@ -10,7 +10,7 @@ const arrayToList = arr => {
   for (let i = 1; i < arr.length; i++) {
     let p = list //pointer start from first node
 
-    //iterate through from first to last node
+    //iterate through list from first to last node
     while (p.rest != null) {
       p = p.rest
     }
@@ -23,39 +23,60 @@ const arrayToList = arr => {
 
 const listToArray = list => {
   let arr = []
-
+  
+  //push the first value of  the list to array
   arr.push(list.value)
+  
+  //Iterate through the list
   while (list.rest != null) {
     list = list.rest
     arr.push(list.value)
   }
+
   return arr
 }
 
 const prepend = (el, list) => {
+
+  //If list is not null, use auxiliary variable to remember the list
   if (list != null) {
     let tmp = list
+
+    //prepend new element to the list
     list = node(el, tmp)
   } else {
+    ///else it's a new list initialized with the parameter's element
     list = node(el, null)
   }
+
   return list
 }
 
-const nth = (list, el) => {
-  let count = 0
-  let p = list
 
-  while (p.rest != null) {
-    if (count == el) {
-      return p.value
-    }
-    count++
-    p = p.rest
-  }
+const nth = (list, pos) => {
+  // let count = 0
+  // let p = list
+  // //Iterate through the list
+  // while (p.rest != null) {
+  //   //Count is the position of the current element
+  //   //If count equals to the passed position => return value
+  //   if (count == pos) {
+  //     return p.value
+  //   }
+  //   //point to next element
+  //   p = p.rest
+  //   //increase the index counter
+  //   count++
+  // }
+
+  //recursive function
+  if (list == null) return undefined
+  else if (pos == 0) return list.value  
+  else return nth(list.rest, pos-1)
 }
+
 
 console.log(arrayToList([10, 20, 30]))
 console.log(listToArray(arrayToList([10, 20, 30])))
 console.log(prepend(10, prepend(20, null)))
-console.log(nth(arrayToList([10, 20, 30]), 4));
+console.log(nth(arrayToList([10, 20, 30, 40, 50]), 2));
